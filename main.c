facdc0357155d8fbdf805a4cccd5a27f9ff851a0
addMemeberYUSHENWANG <michael@utmuser165-133.wireless.utoronto.ca>
2018-01-10 00:55:01 -0500

    first exercise

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSC209
+//
+//  Created by Michael Wang on 2018/1/6.
+//  Copyright © 2018年 Michael Wang. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double maxnum = fmax(1,3);
+    printf("%f",maxnum);
+   
+}
